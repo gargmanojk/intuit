b17253621606cc650ee848442780d3a954cc97b1
Manoj Kumar Garg <gargmanojk@gmail.com>
2025-11-16 15:32:32 -0800

    deleted dead code

--- turbotax-refund-status-service/src/main/java/com/intuit/turbotax/refundstatus/domain/filing/FilingMetadataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.intuit.turbotax.refundstatus.domain.filing;
-
-import java.util.Optional;
-
-public interface FilingMetadataRepository {
-    
-    Optional<FilingMetadata> findLatestByUserId(String userId);
-}
--- turbotax-refund-status-service/src/main/java/com/intuit/turbotax/refundstatus/domain/filing/FilingMetadataService.java
+++
@@ -1,7 +0,0 @@
-package com.intuit.turbotax.refundstatus.domain.filing;
-
-import java.util.Optional;
-
-public interface FilingMetadataService {    
-    Optional<FilingMetadata> findLatestFilingForUser(String userId);
-}
