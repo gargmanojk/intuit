c9559e370a749351e83aca3b4dbda7b6e280a04b
Manoj Kumar Garg <gargmanojk@gmail.com>
2025-11-16 19:33:04 -0800

    some edits

---
+++ turbotax-ai-refund-eta-service/src/main/java/com/intuit/turbotax/aieta/domain/Jurisdiction.java
@@ -0,0 +1,9 @@
+package com.intuit.turbotax.aieta.domain;
+
+public enum Jurisdiction {
+    FEDERAL,
+    STATE_CA,
+    STATE_NY,
+    STATE_NJ
+    // ...
+}
---
+++ turbotax-refund-status-service/src/main/java/com/intuit/turbotax/refundstatus/domain/Jurisdiction.java
@@ -0,0 +1,9 @@
+package com.intuit.turbotax.refundstatus.domain;
+
+public enum Jurisdiction {
+    FEDERAL,
+    STATE_CA,
+    STATE_NY,
+    STATE_NJ
+    // ...
+}
